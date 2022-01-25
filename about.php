d880e5058f8f4e17087785345d8a5ba786112d64
Jasper Vries (Gemeente Den Haag) <jasper.vries@denhaag.nl>
2022-01-25 10:10:38 +0100

    Change: about pagina

--- about.php
+++ about.php
@@ -1,7 +1,7 @@
 <?php 
 /*
  	assetwebsite - viewer en aanvraagformulier voor verkeersmanagementassets
-    Copyright (C) 2020 Gemeente Den Haag, Netherlands
+    Copyright (C) 2020, 2022 Gemeente Den Haag, Netherlands
     Developed by Jasper Vries
  
     This program is free software: you can redistribute it and/or modify
@@ -40,19 +40,19 @@ include('menu.inc.php');
 
 <div id="content">
     <h1>Waarom deze website?</h1>
-    <p>Beheergrenzen zijn voor een weggebruiker niet van belang. Om weggebruikers zo goed mogelijk te informeren is het vaak nodig om DRIPs te gebruiken die worden aangestuurd door verschillende wegbeheerders. Vanuit het samenwerkingsverband <a href="https://www.bereiknu.nl" target="_blank">BEREIK!</a> is daarom deze website gestart voor wegbeheerders om van elkaar te zien wie waar DRIPs heeft staan. Via deze website kan een uniform aanvraagformulier worden gegenereerd dat kan worden ingediend bij de verschillende wegbeheerders. Hierdoor is er voor de aanvrager één centrale plaats met de meest actuele informatie. Gaandeweg is de website uitgebreid met andere wegkantsystemen en wegbeheerders buiten de Zuidvleugel.<p>
+    <p>Beheergrenzen zijn voor een weggebruiker niet van belang. Om weggebruikers zo goed mogelijk te informeren is het vaak nodig om DRIPs te gebruiken die worden aangestuurd door verschillende wegbeheerders. Vanuit het samenwerkingsverband BEREIK! (thans <a href="https://www.zuidhollandbereikbaar.nl/" target="_blank">Zuid-Holland Bereikbaar</a>) is daarom deze website gestart voor wegbeheerders om van elkaar te zien wie waar DRIPs heeft staan. Via deze website kan een uniform aanvraagformulier worden gegenereerd dat kan worden ingediend bij de verschillende wegbeheerders. Hierdoor is er voor de aanvrager één centrale plaats met de meest actuele informatie. Gaandeweg is de website uitgebreid met andere wegkantsystemen en wegbeheerders buiten de Zuidvleugel.<p>
 
     <h1>DRIP-aanvraag</h1>
-    <p>De mogelijkheid tot het maken van een DRIP-aanvraag is onderdeel van een afgestemd werkproces binnen <a href="https://www.bereiknu.nl" target="_blank">BEREIK!</a>. Hoewel DRIPs van wegbeheerders buiten de Zuidvleugel kunnen worden opgenomen, gelden in andere regio's wellicht andere werkprocessen. Het blijft de verantwoordelijkheid van de aanvrager om zijn of haar aanvraag op de juiste manier bij de juiste partijen in te dienen. Uiteraard is het voor andere regio's mogelijk om op de werkwijze van BEREIK! aan te sluiten.</p>
+    <p>De mogelijkheid tot het maken van een DRIP-aanvraag is onderdeel van een afgestemd werkproces binnen <a href="https://www.zuidhollandbereikbaar.nl/" target="_blank">Zuid-Holland Bereikbaar</a>. Hoewel DRIPs van wegbeheerders buiten de Zuidvleugel kunnen worden opgenomen, gelden in andere regio's wellicht andere werkprocessen. Het blijft de verantwoordelijkheid van de aanvrager om zijn of haar aanvraag op de juiste manier bij de juiste partijen in te dienen. Uiteraard is het voor andere regio's mogelijk om op de werkwijze van Zuid-Holland Bereikbaar aan te sluiten.</p>
 
     <h1>Fouten in locatie/details melden</h1>
     <p>De inhoud van de website, waaronder de locaties van de wegkantsystemen en de detailgegevens worden beheerd vanuit de verschillende organisaties. Functionaliteit om fouten te kunnen melden <a href="https://github.com/VCDH/assetwebsite/issues/1" target="_blank">wordt nog toegevoegd</a>.</p>
 
     <h1>Beheer website</h1>
-    <p>Het technisch beheer van deze website wordt namens BEREIK! gedaan door Jasper Vries. Meld technische problemen of wensen in de <a href="https://github.com/VCDH/assetwebsite/issues" target="_blank">issuetracker op GitHub</a>. Kijk eerst of er al een issue bestaat met dezelfde bug of wens alvorens een nieuw issue aan te maken.</p>
+    <p>Het technisch beheer van deze website wordt namens Zuid-Holland Bereikbaar gedaan door Jasper Vries. Meld technische problemen of wensen in de <a href="https://github.com/VCDH/assetwebsite/issues" target="_blank">issuetracker op GitHub</a>. Kijk eerst of er al een issue bestaat met dezelfde bug of wens alvorens een nieuw issue aan te maken.</p>
 
     <h1>Ik wil mijn assets ook op deze website</h1>
-    <p>Lijkt het je als wegbeheerder, binnen of buiten BEREIK!, handig om je assets ook op deze website op te nemen? Neem dan contact op met jasper punt vries apenstaart denhaag punt nl voor de mogelijkheden. Het is ook mogelijk om als regio of individuele wegbeheerder gebruik te gaan maken van het DRIP-aanvraagformulier. Een bestaande lijst met assets kan in &eacute;&eacute;n keer worden ingelezen. Je blijft als wegbeheerder zelf verantwoordelijk voor het bijhouden van je eigen assets en krijgt hiervoor dan de nodige bewerkrechten.</p>
+    <p>Lijkt het je als wegbeheerder, binnen of buiten Zuid-Holland Bereikbaar, handig om je assets ook op deze website op te nemen? Neem dan contact op met jasper punt vries apenstaart denhaag punt nl voor de mogelijkheden. Het is ook mogelijk om als regio of individuele wegbeheerder gebruik te gaan maken van het DRIP-aanvraagformulier. Een bestaande lijst met assets kan in &eacute;&eacute;n keer worden ingelezen. Je blijft als wegbeheerder zelf verantwoordelijk voor het bijhouden van je eigen assets en krijgt hiervoor dan de nodige bewerkrechten.</p>
 
     <h1>Broncode</h1>
     <p>De broncode van deze website is open source en is gepubliceerd op <a href="https://github.com/VCDH/assetwebsite" target="_blank">GitHub</a>.</p>
